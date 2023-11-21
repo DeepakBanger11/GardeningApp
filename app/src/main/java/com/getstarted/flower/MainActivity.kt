0a4368b469d270a2fd99e668f522ab3eef02aa17
Deepak Banger <de4373468@b01s9022.home>
2023-11-21 15:28:15 -0500

    created tabularview and fragments

--- app/src/main/java/com/getstarted/flower/MainActivity.kt
+++ app/src/main/java/com/getstarted/flower/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.getstarted.flower
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.getstarted.flower.tabPagerAdapter.TabPagerAdapter
+import com.getstarted.flower.fragment.MyGardenFragment
+import com.getstarted.flower.fragment.MyPlantsFragment
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView (R.layout.activity_main)
+        val viewPager: ViewPager = findViewById(R.id.viewPager)
+        val tabLayout: TabLayout = findViewById(R.id.tabLayout)
+        val adapter = TabPagerAdapter(supportFragmentManager)
+        adapter.addFragment(MyGardenFragment(), "My Garden")
+        adapter.addFragment(MyPlantsFragment(), "My Plants")
+        viewPager.adapter = adapter
+        tabLayout.setupWithViewPager(viewPager)
     }
 }
---
+++ app/src/main/java/com/getstarted/flower/fragment/MyGardenFragment.kt
@@ -0,0 +1,21 @@
+package com.getstarted.flower.fragment
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.getstarted.flower.R
+
+
+class MyGardenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_my_garden, container, false)
+    }
+}
